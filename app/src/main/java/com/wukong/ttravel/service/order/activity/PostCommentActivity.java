d9ea0e354e91c54d595ba1ddddac30bc3a994053
wukong <caijunwei1990@163.com>
2016-03-13 01:58:49 +0800

    完成发表评论页面.

--- app/src/main/java/com/wukong/ttravel/service/order/activity/PostCommentActivity.java
+++ app/src/main/java/com/wukong/ttravel/service/order/activity/PostCommentActivity.java
@@ -1,6 +1,9 @@
 package com.wukong.ttravel.service.order.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -11,13 +14,13 @@ import com.wukong.ttravel.Base.views.tagview.Tag;
 import com.wukong.ttravel.Base.views.tagview.TagListView;
 import com.wukong.ttravel.Base.views.tagview.TagView;
 import com.wukong.ttravel.R;
-import com.wukong.ttravel.service.home.model.Comment;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by wukong on 3/11/16.
@@ -27,11 +30,70 @@ public class PostCommentActivity extends BaseActivity {
     private List<String> mList;
     private final String[] titles = {"怀念我的母校","家乡","和我最亲爱的朋友"};
     private final List<Tag> mTags = new ArrayList<Tag>();
+    private int seletedStarLevel = 0;
 
     @Bind(R.id.tagview)
     TagListView tagListView;
 
+    @Bind(R.id.comment_content)
+    EditText commentContent;
 
+    @Bind(R.id.star0)
+    ImageView star0;
+    @Bind(R.id.star1)
+    ImageView star1;
+    @Bind(R.id.star2)
+    ImageView star2;
+    @Bind(R.id.star3)
+    ImageView star3;
+    @Bind(R.id.star4)
+    ImageView star4;
+
+    @OnClick(R.id.star0)
+    void OnStar0Click(View v) {
+        seletecStarLevel(0);
+    }
+    @OnClick(R.id.star1)
+    void OnStar1Click(View v) {
+        seletecStarLevel(1);
+    }
+    @OnClick(R.id.star2)
+    void OnStar2Click(View v) {
+        seletecStarLevel(2);
+    }
+    @OnClick(R.id.star3)
+    void OnStar3Click(View v) {
+        seletecStarLevel(3);
+    }
+    @OnClick(R.id.star4)
+    void OnStar4Click(View v) {
+        seletecStarLevel(4);
+    }
+
+    @OnClick(R.id.do_comment_button)
+    void OnFinishButtonClick(View v) {
+
+        //先判断评论内容
+        String string = commentContent.getText().toString();
+        if (string.length() < 10) {
+            showInfo("评论内容不能少于10个字哦~");
+            return;
+        }
+
+        //获取星级
+        System.out.println("start---level-->" + seletedStarLevel);
+
+        //获取用户选择标签
+        for (int i = 0; i < mTags.size();i ++) {
+            Tag tag = mTags.get(i);
+            if (tag.isChecked()) {
+                System.out.println("tagId :" + tag.getTagId() + "\ntagName: " + tag.getTitle());
+            }
+
+        }
+
+        ///TO DO 发起网络请求
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +111,7 @@ public class PostCommentActivity extends BaseActivity {
         });
 
     }
+
     void loadData() {
 
         showLoading("正在获取标签数据");
@@ -93,6 +156,54 @@ public class PostCommentActivity extends BaseActivity {
             params = null;
         }
         return params;
+    }
+
+    void seletecStarLevel(int i) {
+        switch (i) {
+            case 0:
+                star0.setImageResource(R.drawable.home_star_active);
+                star1.setImageResource(R.drawable.home_star_inactive);
+                star2.setImageResource(R.drawable.home_star_inactive);
+                star3.setImageResource(R.drawable.home_star_inactive);
+                star4.setImageResource(R.drawable.home_star_inactive);
+                seletedStarLevel = 1;
+                break;
+            case 1:
+                star0.setImageResource(R.drawable.home_star_active);
+                star1.setImageResource(R.drawable.home_star_active);
+                star2.setImageResource(R.drawable.home_star_inactive);
+                star3.setImageResource(R.drawable.home_star_inactive);
+                star4.setImageResource(R.drawable.home_star_inactive);
+                seletedStarLevel = 2;
+                break;
+            case 2:
+                star0.setImageResource(R.drawable.home_star_active);
+                star1.setImageResource(R.drawable.home_star_active);
+                star2.setImageResource(R.drawable.home_star_active);
+                star3.setImageResource(R.drawable.home_star_inactive);
+                star4.setImageResource(R.drawable.home_star_inactive);
+                seletedStarLevel = 3;
+                break;
+            case 3:
+                star0.setImageResource(R.drawable.home_star_active);
+                star1.setImageResource(R.drawable.home_star_active);
+                star2.setImageResource(R.drawable.home_star_active);
+                star3.setImageResource(R.drawable.home_star_active);
+                star4.setImageResource(R.drawable.home_star_inactive);
+                seletedStarLevel = 4;
+                break;
+            case 4:
+                star0.setImageResource(R.drawable.home_star_active);
+                star1.setImageResource(R.drawable.home_star_active);
+                star2.setImageResource(R.drawable.home_star_active);
+                star3.setImageResource(R.drawable.home_star_active);
+                star4.setImageResource(R.drawable.home_star_active);
+                seletedStarLevel = 5;
+                break;
+            default:
+                break;
+        }
+
     }
 
 }
